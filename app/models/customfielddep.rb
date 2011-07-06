f13875c6f87e956e12c850e7ef8063948236f6e1
mainframezen <ribeirolog@gmail.com>
2011-07-06 15:45:29 +0100

    working, next is YAML copying to possible values

--- app/models/customfielddep.rb
+++ app/models/customfielddep.rb
@@ -4,19 +4,6 @@ class Customfielddep < ActiveRecord::Base
   validates_uniqueness_of :custom_master_id, :custom_slave_id
 
 
-  def yaml(obj=nil)
-      read_attribute :yaml
-  end
-
-  # Makes possible_values accept a multiline string
-  def yaml=(arg)
-    if arg.is_a?(Array)
-      write_attribute(:yaml, arg.compact.collect(&:strip).select {|v| !v.blank?})
-    else
-      self.yaml = arg.to_s.split(/[\n\r]+/)
-    end
-  end
-
 
   
 end
