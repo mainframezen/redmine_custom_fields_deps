a1a7c03185299e34c6c88a3edc91f5e486cfa4db
mainframezen <ribeirolog@gmail.com>
2011-07-06 12:23:06 +0100

    save works?

--- app/controllers/customfieldsdeps_controller.rb
+++ app/controllers/customfieldsdeps_controller.rb
@@ -14,11 +14,20 @@ class CustomfieldsdepsController < ApplicationController
   
 
   def save
+	@custdep = Customfielddep.new(params[:customfielddep])
+	if @custdep.save
+		flash[:notice] = l(:notice_successful_save)
+      		redirect_to :action => "list", :id => params[:id]
+	else
+		flash[:notice] = l(:notice_unsuccessful_save)
+      		redirect_to :action => "new", :id => params[:id]
+	end
      
   end
   
   def new
-    
+    @custdep = Customfielddep.new
+    @customs = CustomField.find(:all,:conditions=>"type='IssueCustomField'",:order=>"name")
   end
   
 end
--- app/models/customfielddep.rb
+++ app/models/customfielddep.rb
@@ -1,5 +1,7 @@
 class Customfielddep < ActiveRecord::Base
   unloadable
 
+  belongs_to :custom_fields, :class_name => 'CustomField', :foreign_key => 'custom_master_id'
+  
   
 end
